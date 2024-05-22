5fa5ec200a40e52792573b3a59a80bcd1ee0780c
Maxim Gerasimenko <maximgerasimenko@bk.ru>
2024-05-23 00:22:47 +0300

    Стартер пак для создания проектов Стек: Next.js, TypeScript, Tailwind, shadcn-ui, Zustand, Tanstack Query

---
+++ .vscode/cch-template/React FC TS/{{camelCase}}/{{pascalCase}}.tsx
@@ -0,0 +1,9 @@
+import { I{{pascalCase}} } from './{{pascalCase}}.types'
+
+export default function {{pascalCase}}({}: I{{pascalCase}}) {
+  return (
+    <div className="flex">
+      
+    </div>
+  )
+}
